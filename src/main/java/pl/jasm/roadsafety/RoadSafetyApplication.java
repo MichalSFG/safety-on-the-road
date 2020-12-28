d242d101f8caef6e2f71a7271e4c51d6548cf0a0
MichalSFG <mickeyjas80@gmail.com>
2020-12-28 15:20:51 +0100

    hello new app

---
+++ src/main/java/pl/jasm/roadsafety/RoadSafetyApplication.java
@@ -0,0 +1,13 @@
+package pl.jasm.roadsafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadSafetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadSafetyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jasm/roadsafety/RoadSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jasm.roadsafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadSafetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
